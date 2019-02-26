da8122f5ae73e86113b27e336140d29dd5bf61dd
chiranz <chiranpoudyal@gmail.com>
2019-02-26 08:14:40 +0530

    settings development and production

---
+++ README.md
@@ -0,0 +1,18 @@
+# DJANGO PROJECT BOILERPLATE
+
+Django project boilerplate for quick start
+
+
+Steps:
+
+1. Pull this repo
+2. Create a virtualenv and install dependencies with `pip install -r requirements.txt`
+3. Configure your .env variables
+4. Rename your project with `python manage.py rename yourprojectname`
+
+This project includes:
+
+1. Django Debug Toolbar
+2. Settings for both development and production
+3. Python-decouple for securely referencing your private keys and data
+4. Custom django command to rename project
---
+++ bp/settings/production.py
@@ -0,0 +1,34 @@
+from .base import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['my_site_ip', 'my_site']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
